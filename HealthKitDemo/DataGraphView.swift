b3e784ea3913162581be6364047385485a91c006
AGart <A_Gartenberg@yahoo.com>
2014-09-23 12:23:56 -0400

    third commit

---
+++ HealthKitDemo/DataGraphView.swift
@@ -0,0 +1,22 @@
+//
+//  DataGraphView.swift
+//  HealthKitDemo
+//
+//  Created by Albee Gartenberg on 9/22/14.
+//  Copyright (c) 2014 BitFountain. All rights reserved.
+//
+
+import UIKit
+
+class DataGraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
